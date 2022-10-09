e80db02b8e39aaae708e13809dde0b8bd421880c
Marcin <marcin@o2.pl>
2022-10-09 18:36:49 +0100

    woo

--- src/LEVEL/FIRST LEVEL/monsterOnFirstLevel.js
+++ src/LEVEL/FIRST LEVEL/monsterOnFirstLevel.js
@@ -9,18 +9,18 @@ import { Zombie } from "../../objects/MONSTER/zombie/zombie"
 export const monsterOnFirstLevel=()=>{
     let array=[
 
-    // createMonster(Sheep,1,[{x:335,y:467}]),
-    // createMonster(Sheep,1,[{x:335,y:67}]),
-    // createMonster(Sheep,1,[{x:1135,y:467}]),
-    // createMonster(Zombie,1,[{x:1215,y:467}]),
-    // createMonster(Zombie,1,[{x:1115,y:467}]),
-    // createMonster(Zombie,1,[{x:1015,y:467}]),
+    createMonster(Sheep,1,[{x:335,y:467}]),
+    createMonster(Sheep,1,[{x:335,y:67}]),
+    createMonster(Sheep,1,[{x:1135,y:467}]),
+    createMonster(Zombie,1,[{x:1215,y:467}]),
+    createMonster(Zombie,1,[{x:1115,y:467}]),
+    createMonster(Zombie,1,[{x:1015,y:467}]),
  
    
 
-    // createMonster(Ghost,1,[{x:15,y:167}]),
-    // createMonster(Ghost,1,[{x:605,y:167}]),
-    // createMonster(Soldier,1,[{x:605,y:367}]),
+    createMonster(Ghost,1,[{x:15,y:167}]),
+    createMonster(Ghost,1,[{x:605,y:167}]),
+    createMonster(Soldier,1,[{x:605,y:367}]),
     
  
   
